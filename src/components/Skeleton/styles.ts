e883da74f98b38202643d7f2f9f1af6f2138d01c
eduardo <juniorcose@gmail.com>
2023-01-31 14:32:02 -0300

    fix

--- src/components/Skeleton/styles.ts
+++ src/components/Skeleton/styles.ts
@@ -4,6 +4,13 @@ export const ContainerCard = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 22px;
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 650px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 export const Cards = styled.div`
